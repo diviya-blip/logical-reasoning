2168e397da5ade489a78f94a4e928a72805a2f35
diviya-blip <divijai059@gmail.com>
2026-08-13 11:56:05 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number of terms: "))
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    next_number = a + b
+    a = b
+    b = next_number
